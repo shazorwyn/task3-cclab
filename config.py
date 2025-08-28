d8e5a6d1c1d9906643d734025ba4e59ae4551c63
Aditya Kumar Gupta <adityakumar.gupta.cd.eee22@itbhu.ac.in>
2025-08-28 21:33:43 +0530

    Added connection string set up

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
-STORAGE_CONNECTION_STRING = "SECRET"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+STORAGE_CONNECTION_STRING = os.getenv("STORAGE_CONNECTION_STRING")
+SECRET_KEY = os.getenv("SECRET_KEY")
 CONTAINER_NAME = "container01"
